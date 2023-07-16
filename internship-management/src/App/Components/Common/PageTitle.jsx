f335bac48188a07df9715a0b84f5040da2065511
Sumit Kawale <125868529+sumitkawale2@users.noreply.github.com>
2023-07-17 00:25:52 +0530

    Delete internship-management directory

--- internship-management/src/App/Components/Common/PageTitle.jsx
+++
@@ -1,8 +0,0 @@
-import "../../css/common.css";
-
-export default function PageTitle(props) {
-    return <div className="pageTitle">
-        {props.children}
-        <span>{props.title}</span>
-    </div>
-}
